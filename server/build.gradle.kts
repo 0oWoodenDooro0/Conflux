5914604e52b939e42914318c61fd4365362b3bbe
Vincent <vincent031525@gmail.com>
2026-05-01 22:45:02 +0800

    feat(server): enable CORS for web app compatibility

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation(libs.ktor.serverCore)
     implementation(libs.ktor.serverNetty)
     implementation(libs.ktor.serverContentNegotiation)
+    implementation(libs.ktor.serverCors)
     implementation(libs.ktor.serializationKotlinxJson)
     implementation(libs.exposed.core)
     implementation(libs.exposed.dao)
--- server/src/main/kotlin/website/woodendoor/conflux/Application.kt
+++ server/src/main/kotlin/website/woodendoor/conflux/Application.kt
@@ -4,7 +4,9 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -25,6 +27,13 @@ fun main() {
 fun Application.module() {
     install(ContentNegotiation) {
         json()
+    }
+    install(CORS) {
+        anyHost() // In production, replace with specific hosts
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
     }
     DatabaseFactory.init()
     transaction {
