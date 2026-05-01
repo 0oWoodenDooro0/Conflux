85f90f2c5aab714d3f0df857153a6257bb76ebee
Vincent <vincent031525@gmail.com>
2026-05-01 22:06:27 +0800

    feat(server): Integrate DatabaseFactory initialization into Ktor module

--- server/src/main/kotlin/website/woodendoor/conflux/Application.kt
+++ server/src/main/kotlin/website/woodendoor/conflux/Application.kt
@@ -6,12 +6,22 @@ import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+import website.woodendoor.conflux.database.DatabaseFactory
+import website.woodendoor.conflux.database.models.*
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    DatabaseFactory.init()
+    transaction {
+        SchemaUtils.create(Users, Servers, Roles, Channels, ServerMembers)
+    }
+    
     routing {
         get("/") {
             call.respondText("Ktor: ${Greeting().greet()}")
