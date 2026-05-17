d1cb93db66a1da62585bdddb8e94c7db6fb08d49
Vincent <vincent031525@gmail.com>
2026-05-17 11:03:38 +0800

    refactor: add @everyone role constants and remove legacy role names

--- shared/src/commonMain/kotlin/website/woodendoor/conflux/Constants.kt
+++ shared/src/commonMain/kotlin/website/woodendoor/conflux/Constants.kt
@@ -3,5 +3,5 @@ package website.woodendoor.conflux
 const val SERVER_PORT = 8080
 const val DEFAULT_BASE_URL = "http://localhost:$SERVER_PORT"
 
-const val DEFAULT_ROLE_NAME_OWNER = "Owner"
-const val DEFAULT_ROLE_NAME_MEMBER = "Member"
+const val DEFAULT_ROLE_NAME_EVERYONE = "@everyone"
+const val DEFAULT_ROLE_PRIORITY_EVERYONE = -1
